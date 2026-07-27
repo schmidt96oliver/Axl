c2b90665e5057c236d24867ad654eace9ad4c0d7
Oliver Schmidt <schmidt.96.oliver@gmail.com>
2026-07-27 18:59:02 +0200

    fix: remove INumber constraint on Guard.InRange
    Its not needed and nice to use it for other conditions as well.

--- src/Axl.Compiler/Guard.cs
+++ src/Axl.Compiler/Guard.cs
@@ -14,7 +14,6 @@ internal static class Guard
         bool condition, 
         [CallerArgumentExpression(nameof(arg))] string? paramName = null,
         [CallerArgumentExpression(nameof(condition))] string? expected = null)
-        where T: INumber<T>
     {
         if (!condition)
             throw new ArgumentOutOfRangeException(paramName, arg, $"{paramName} was out of range, expected {expected}.");
