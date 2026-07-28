98791fcf1ddabc3096e85b98e06a7a683ae1a1c1
Oliver Schmidt <schmidt.96.oliver@gmail.com>
2026-07-28 10:41:49 +0200

    test: print input and expected token output
    This is a little better to debug failed tests, since its directly visible in the test explorer.

--- tests/Axl.Compiler.UnitTests/Taxl/TaxlLexerTests.cs
+++ tests/Axl.Compiler.UnitTests/Taxl/TaxlLexerTests.cs
@@ -7,7 +7,11 @@ public partial class TaxlLexerTests(ITestOutputHelper output)
 {
     public static void AssertKindsIgnoreTrivia(string input, params TaxlTokenKind[] expectedKinds)
     {
+        TestContext.Current.TestOutputHelper?.WriteLine("--- Input ---");
         TestContext.Current.TestOutputHelper?.WriteLine(input);
+        TestContext.Current.TestOutputHelper?.WriteLine("--- Expected Tokens (without trivia) ---");
+        TestContext.Current.TestOutputHelper?.WriteLine(string.Join(", ", expectedKinds));
+        
         
         var diagnostics = new DiagnosticBag();
         var lex = TaxlLexer.Lex(SourceView.Whole(SourceFile.FromText(input)), diagnostics);
