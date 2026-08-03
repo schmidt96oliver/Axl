97da8920655768df512f7fe51793b50be816faba
Oliver Schmidt <schmidt.96.oliver@gmail.com>
2026-08-03 13:59:56 +0200

    feat: split Lsp and Playground into different command-line args
    That was very necessary! Switching between them manually by commenting was tedious.

---
+++ src/Axl/Playground.cs
@@ -0,0 +1,49 @@
+﻿using Axl.Compiler;
+using Axl.Compiler.Diagnostics;
+using Axl.Compiler.Syntax;
+
+namespace Axl;
+
+public static class Playground
+{
+    private static string TestFilePath = Path.Combine("..", "..", "..", "..", "src", "Axl", "test.axl");
+
+    public static void Run()
+    {
+        var prevText = "";
+        while (true)
+        {
+            Thread.Sleep(250);
+
+            var source = SourceFileView.FromFile(TestFilePath);
+            if (source.File.Text == prevText)
+                continue;
+
+            prevText = source.File.Text;
+            
+            Console.Clear();
+            ExecuteTestFile(source);
+        }
+    }
+
+    private static void ExecuteTestFile(SourceFileView source)
+    {
+        var bag = new DiagnosticBag();
+        var lex = Lexer.Lex(source, bag);
+
+        // Print diagnostics
+        if (bag.Diagnostics.Count > 0)
+        {
+            foreach (var diag in bag.Diagnostics)
+                Console.WriteLine(
+                    $"{diag.DefaultSeverity.ToString().ToUpper()} {diag.Id}@{diag.Location.Span}: {diag.Message}");
+        }
+
+        // Print tokens
+        foreach (var token in lex/*.Where(t => t.Kind is not (TokenKind.Whitespace or TokenKind.Comment))*/)
+        {
+            var text = source.GetText(token.Span).ToString().Replace("\n", "\\n");
+            Console.WriteLine($"- {token.Kind}: \"{text}\"");
+        }
+    }
+}
