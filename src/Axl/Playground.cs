a9e6624d9df374bebba3427c1aa24b507861db1e
Oliver Schmidt <schmidt.96.oliver@gmail.com>
2026-08-09 11:10:07 +0200

    feat: add Spectre.Console for pretty playground

--- src/Axl/Playground.cs
+++ src/Axl/Playground.cs
@@ -1,6 +1,7 @@
 ﻿using Axl.Compiler;
 using Axl.Compiler.Diagnostics;
 using Axl.Compiler.Syntax;
+using Spectre.Console;
 
 namespace Axl;
 
@@ -23,8 +24,7 @@ public static class Playground
             }
             catch (IOException e)
             {
-                Console.Clear();
-                Console.WriteLine(e);
+                AnsiConsole.WriteException(e);
                 continue;
             }
             
@@ -33,72 +33,108 @@ public static class Playground
             
             prevText = source.File.Text;
             
-            Console.Clear();
-            ExecuteTestFile(source);
+            AnsiConsole.Clear();
+            RenderSpectreFile(source);
         }
     }
 
-    private static void ExecuteTestFile(SourceFileView source)
+    private static void RenderSpectreFile(SourceFileView source)
     {
-        var tree = Parser.Parse(source);
+        var syntaxTree = Parser.Parse(source);
         
-        // Print diagnostics
-        Console.ForegroundColor = ConsoleColor.White;
-        foreach (var diag in tree.Diagnostics)
+        // Diagnostics:
+        foreach (var diagnostic in syntaxTree.Diagnostics)
         {
-            var spans = string.Join(", ", diag.Locations.Select(location => location.Span));
-            Console.WriteLine(
-                $"{diag.DefaultSeverity.ToString().ToUpper()} {diag.Id}@{spans}: {diag.Message}");
-            foreach (var related in diag.Related)
-                Console.WriteLine($"   related@{related.Location.Span}: {related.Label}");
-        }
-        
-        Console.WriteLine("//=== tree");
-        
-        Print(tree.Root, "");
-        return;
-        
-        void Print(SyntaxElement element, string prefix)
-        {
-            switch (element)
+            var markup = diagnostic.DefaultSeverity switch
             {
-                case Token { Kind.IsTrivia: false } token:
+                DiagnosticSeverity.Error => "[bold Red]",
+                DiagnosticSeverity.Warning => "[bold Gold1]",
+                _ => "[bold SteelBlue1]"
+            };
+
+            AnsiConsole.Markup($"{markup}{diagnostic.Id}[/] ");
+            var locationStrs = string.Join(" ", diagnostic.Locations.Select(GetLocationText));
+            AnsiConsole.Write(locationStrs);
+            AnsiConsole.WriteLine($":  {diagnostic.Message}");
+            foreach (var relatedLabel in diagnostic.Related)
+            {
+                AnsiConsole.MarkupInterpolated($"  [underline]related[/]@{GetLocationText(relatedLabel.Location)}: {relatedLabel.Label}");
+            }
+        }
+
+        AnsiConsole.WriteLine();
+        
+        AnsiConsole.Write(BuildTree(syntaxTree, source));
+    }
+
+    private static string GetLocationText(SourceLocation location)
+    {
+        var startLinePos = location.File.GetLinePositionOrEof(location.Span.First);
+        var endLinePos = location.File.GetLinePositionOrEof(location.Span.End);
+        
+        if (startLinePos.Line == endLinePos.Line)
+            return $"l.{startLinePos.Line} @ {startLinePos.Column}-{endLinePos.Column}";
+
+        return $"l.{startLinePos.Line}@{startLinePos.Column} - l.{endLinePos.Line}@{endLinePos.Column}";
+    }
+
+    private static Tree BuildTree(SyntaxTree syntaxTree, SourceFileView source)
+    {
+        var tree = new Tree("Root");
+        
+        AddChildren(tree, syntaxTree.Root, syntaxTree, source, onErrorNode: false);
+        return tree;
+    }
+    
+    private static void AddChildren(IHasTreeNodes treeNode, SyntaxNode node, SyntaxTree syntaxTree, SourceFileView source, bool onErrorNode)
+    {
+        foreach (var child in node.Children)
+        {
+            if (child is Token { Kind.IsTrivia: false } token)
+            {
+                treeNode.AddNode(GetTokenText(token, onErrorNode));
+            }
+            else if (child is SyntaxNode childNode)
+            {
+                var color = onErrorNode ? "[red]" :
+                    childNode.Kind is SyntaxKind.Error ? "[bold red]" : "[bold green]";
+                
+                var nonTrivia = childNode.Children
+                    .Where(t => t is Token {Kind.IsTrivia: false} or SyntaxNode)
+                    .ToList();
+                if (nonTrivia.All(el => el is Token))
                 {
-                    var text = source.GetText(token.Span).ToString().Replace("\n", "\\n").Replace("\r", "\\r");
-                    Console.ForegroundColor = ConsoleColor.DarkGray;
-                    Console.Write(prefix);
-                    Console.ForegroundColor = ConsoleColor.White;
-                    Console.Write($"{token.Kind}: ");
-                    Console.ForegroundColor = ConsoleColor.DarkRed;
-                    Console.WriteLine($"\"{text}\"");
-                    break;
+                    var text = $"{color}{childNode.Kind}[/] ";
+                    
+                    text += string.Join(" ", nonTrivia
+                        .OfType<Token>()
+                        .Select(t => GetTokenText(t, onErrorNode || childNode.Kind is SyntaxKind.Error)));
+                    
+                    treeNode.AddNode(text);
+                    continue;
                 }
                 
-                case SyntaxNode node:
-                {
-                    Console.ForegroundColor = ConsoleColor.DarkGray;
-                    Console.Write(prefix);
-                    Console.ForegroundColor = ConsoleColor.DarkGreen;
-                    Console.Write($"{node.Kind}");
-
-                    var nonTriviaTokens = node.Children
-                        .Where(t => t is Token {Kind.IsTrivia: false} or SyntaxNode)
-                        .ToList();
-                    if (nonTriviaTokens is [Token token])
-                    {
-                        Console.ForegroundColor = ConsoleColor.DarkRed;
-                        var text = source.GetText(token.Span).ToString().Replace("\n", "\\n").Replace("\r", "\\r");
-                        Console.WriteLine($": \"{text}\"");
-                    }
-                    else
-                    {
-                        Console.WriteLine();
-                        foreach (var child in node.Children)
-                            Print(child, prefix + "| ");
-                    }
-                    break;
-                }
+                var childTree = treeNode.AddNode($"{color}{childNode.Kind}[/]");
+                AddChildren(childTree, childNode, syntaxTree, source, onErrorNode: childNode.Kind is SyntaxKind.Error);
             }
+        }
+
+        string GetTokenText(Token token, bool onErrorNode)
+        {
+            var hasError = token.Span.Length != 0 && syntaxTree.Diagnostics.Any(diag =>
+                diag.DefaultSeverity is DiagnosticSeverity.Error &&
+                diag.Locations.Any(location =>
+                    location.Span.Contains(token.Span.First) || location.Span.Contains(token.Span.End - 1)));
+            var hasEndError = syntaxTree.Diagnostics.Any(diag =>
+                diag.DefaultSeverity is DiagnosticSeverity.Error &&
+                diag.Locations.Any(location => location.Span.First == token.Span.End));
+            var text = source.GetText(token.Span);
+            return string.Concat(hasError
+                    ? $"[underline red]'{text}'[/]"
+                    : onErrorNode
+                        ? $"[red]'{text}'[/]"
+                        : $"[khaki1]'{text}'[/]",
+                hasEndError ? $"[underline red]?[/]" : "");
         }
     }
 }
