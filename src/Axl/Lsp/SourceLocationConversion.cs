00032bd9eea3c6e005be3365adc577b3ea1f301e
Oliver Schmidt <schmidt.96.oliver@gmail.com>
2026-08-12 09:26:25 +0200

    feat: report empty spans to LSP by extending one character instead of leaving it up to the editor
    It looks much prettier that way. Editors squiggly the entire word, which looked ugly.

--- src/Axl/Lsp/SourceLocationConversion.cs
+++ src/Axl/Lsp/SourceLocationConversion.cs
@@ -10,7 +10,32 @@ public static class SourceLocationConversion
         public Range ToLsp()
         {
             var (startLine, startCol) = location.File.GetLinePositionOrEof(location.Span.First);
-            var (endLine, endCol) = location.File.GetLinePositionOrEof(location.Span.End);
+
+            var isEndAtEof = location.Span.End >= location.File.Text.Length;
+            
+            LineInfo? endLineInfo = !isEndAtEof ? location.File.GetLineAt(location.Span.End) : null;
+            var (endLine, endCol) = endLineInfo is LineInfo info
+                ? (info.LineNumber, location.Span.End - info.Span.First)
+                : (location.File.EofLinePosition.Line, location.File.EofLinePosition.Column);
+
+            // --- Special-case empty spans
+            // To prevent the editor snapping back to the previous word, we need to
+            // extend the range by one character. If that is out of the line range,
+            // snap back one character. If that is also not possible, report the 
+            // empty span and leave it to the editor.
+            if (location.Span.Length == 0)
+            {
+                if (isEndAtEof && startCol > 0)
+                        startCol--;
+                else if (!isEndAtEof)
+                {
+                    if (endCol < endLineInfo!.Value.LengthWithoutEnding)
+                        endCol++;
+                    else if (startCol > 0)
+                        startCol--;
+                }
+            }
+            
             return new Range(startLine, startCol, endLine, endCol);
         }
     }
