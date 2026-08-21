652bee7b7c119378c3554f7ed8dea2ebaf55f6a8
Oliver Schmidt <schmidt.96.oliver@gmail.com>
2026-08-21 10:25:11 +0200

    fix: accept any expr in else clause ast instead of rejecting `if`

--- src/Axl.Compiler/Syntax/Tree/Exprs/ElseClauseSyntax.cs
+++ src/Axl.Compiler/Syntax/Tree/Exprs/ElseClauseSyntax.cs
@@ -5,5 +5,5 @@ namespace Axl.Compiler.Syntax.Tree;
 public sealed class ElseClauseSyntax(ImmutableArray<SyntaxElement> children)
     : ExprSyntax(SyntaxKind.ElseClause, children)
 {
-    public BodySyntax Body => Children.FirstOfType<BodySyntax>();
+    public ExprSyntax Body => Children.FirstOfType<ExprSyntax>();
 }
