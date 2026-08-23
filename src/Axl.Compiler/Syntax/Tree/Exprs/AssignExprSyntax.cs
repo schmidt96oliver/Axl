21378a84797345944c0751b821dd2b5b35c2b2db
Oliver Schmidt <schmidt.96.oliver@gmail.com>
2026-08-23 11:27:42 +0200

    feat: add operator to assignment ast
    The binder will need it, since they have different semantics.

--- src/Axl.Compiler/Syntax/Tree/Exprs/AssignExprSyntax.cs
+++ src/Axl.Compiler/Syntax/Tree/Exprs/AssignExprSyntax.cs
@@ -6,5 +6,6 @@ public sealed class AssignExprSyntax(ImmutableArray<SyntaxElement> children)
     : ExprSyntax(SyntaxKind.AssignExpr, children)
 {
     public ExprSyntax Target => Children.FirstOfType<ExprSyntax>();
+    public Token Operator => Children.FirstNonTriviaToken();
     public ExprSyntax Value => Children.SecondOfType<ExprSyntax>();
 }
